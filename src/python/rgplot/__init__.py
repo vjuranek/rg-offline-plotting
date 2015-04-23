33b27fe405cb88a067f44ac745ff1f19723c703f
Vojtech Juranek <vjuranek@redhat.com>
2015-04-23 15:29:23 +0200

    Move to separate module

---
+++ src/python/rgplot/__init__.py
@@ -0,0 +1,12 @@
+from BarChart import BarChart
+from RgMeasurement import RgMeasurement
+from RgReport import RgReport
+from RgVars import GET, GET_NULL
+
+__all__ = [
+    'BarChart',
+    'RgMeasurement',
+    'RgReport',
+    'GET',
+    'GET_NULL'
+]
