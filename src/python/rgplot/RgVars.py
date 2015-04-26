c9f131383739fae8f71608773308385e3010bf1b
Vojtech Juranek <vjuranek@redhat.com>
2015-04-26 21:56:22 +0200

    Add actual throughput variables

--- src/python/rgplot/RgVars.py
+++ src/python/rgplot/RgVars.py
@@ -1,8 +1,43 @@
 class RgVar:
-    pass
+    @classmethod
+    def title(cls):
+        return cls.rg_name.replace(".", " ")
 
 class MRT(RgVar):
     ylabel = 'Mean response time [ms]'
+    rg_name = ""
+    @classmethod
+    def title(cls):
+        return cls.rg_name.replace(".", " ") + " Mean response time" # TODO super(MRT).title() ?? failss with TypeError
+
+class GET(MRT):
+    rg_name = 'BasicOperations.Get'
 
 class GET_NULL(MRT):
     rg_name = 'BasicOperations.Get.Null'
+
+class PUT(MRT):
+    rg_name = 'BasicOperations.Put'
+
+class REMOVE(MRT):
+    rg_name = 'BasicOperations.Remove'
+
+
+
+class AT(RgVar):
+    ylabel = 'Operations per second'
+    @classmethod
+    def title(cls):
+        return cls.rg_name.replace(".", " ") + " Actual Throughput" # TODO super(MRT).title()
+
+class GET_AT(AT, GET):
+    pass
+
+class GET_NULL_AT(AT, GET_NULL):
+    pass
+
+class PUT_AT(AT, PUT):
+    pass
+
+class REMOVE_AT(AT, REMOVE):
+    pass
--- src/python/rgplot/__init__.py
+++ src/python/rgplot/__init__.py
@@ -1,12 +1,20 @@
 from BarChart import BarChart
+from LineChart import LineChart
 from RgMeasurement import RgMeasurement
 from RgReport import RgReport
-from RgVars import GET, GET_NULL
+from RgVars import GET, GET_NULL, PUT, REMOVE, GET_AT, GET_NULL_AT, PUT_AT, REMOVE_AT
 
 __all__ = [
     'BarChart',
+    'LineChart',
     'RgMeasurement',
     'RgReport',
     'GET',
-    'GET_NULL'
+    'GET_NULL',
+    'PUT',
+    'REMOVE',
+    'GET_AT',
+    'GET_NULL_AT',
+    'PUT_AT',
+    'REMOVE_AT'
 ]
