251d7de03a8196d12f0f43248c607f104f22bbb7
Vojtech Juranek <vjuranek@redhat.com>
2015-05-12 22:30:19 +0200

    Add some docs

--- src/python/rgplot/RgVars.py
+++ src/python/rgplot/RgVars.py
@@ -4,27 +4,33 @@ class RgVar:
         return cls.rg_name.replace(".", " ")
 
 class MRT(RgVar):
-    ylabel = 'Mean response time [ms]'
+    '''Mean response time'''
+    ylabel = 'Mean response time [ms]' # in RG in nano seconds, but RgReport converts it to miliseconds
     rg_name = ""
     @classmethod
     def title(cls):
         return cls.rg_name.replace(".", " ") + " Mean response time" # TODO super(MRT).title() ?? failss with TypeError
 
 class GET(MRT):
+    '''Mean response time of GET operation'''
     rg_name = 'BasicOperations.Get'
 
 class GET_NULL(MRT):
+    #TODO what exactly this variable standas for?
     rg_name = 'BasicOperations.Get.Null'
 
 class PUT(MRT):
+    '''Mean response time of PUT operation'''
     rg_name = 'BasicOperations.Put'
 
 class REMOVE(MRT):
+    '''Mean response time of REMOVE operation'''
     rg_name = 'BasicOperations.Remove'
 
 
 
 class AT(RgVar):
+    '''Actual throughtput'''
     ylabel = 'Operations per second'
     @classmethod
     def title(cls):
